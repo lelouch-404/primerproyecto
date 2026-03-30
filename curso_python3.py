dfe3d0228b4bb26868b83d9a09951aaf9b69dd23
lelouch-404 <leodeiska@gmail.com>
2026-03-30 12:44:06 -0700

    pimera mejora de interfaZ

--- curso_python3.py
+++ curso_python3.py
@@ -7,25 +7,33 @@ ventana.geometry("300x200")
 
 
 label_nombre = tk.Label(ventana, text="Nombre:")
-label_nombre.pack()
+label_nombre.grid(row=0, column=0)
 
 entrada_nombre = tk.Entry(ventana)
-entrada_nombre.pack()
+entrada_nombre.grid(row=0,column=1)
 
 label_edad = tk.Label(ventana, text="Edad:")
-label_edad.pack()
+label_edad.grid(row=1,column=0)
 
 entrada_edad = tk.Entry(ventana)
-entrada_edad.pack()
+entrada_edad.grid(row=1,column=1)
 
 resultado = tk.Label(ventana, text="")
-resultado.pack()
+resultado.grid(row=2, column=0, columnspan=2)
 
 def verificar():
     nombre = entrada_nombre.get()
 
+    if nombre.strip() == "":
+        resultado.config(text="Ingresa un nombre válido")
+        return
+
 
     edad = entrada_edad.get()
+
+    if edad.strip() == "":
+        resultado.config(text="ingrese un numero valido")
+        return
 
     try:
         edad = int(edad)
@@ -40,6 +48,6 @@ def verificar():
 
 
 boton = tk.Button(ventana, text="Verificar", command=verificar)
-boton.pack()
+boton.grid(row=3,column=0,columnspan=2,pady=10)
 
 ventana.mainloop()
