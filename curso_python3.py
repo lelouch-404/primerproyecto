fb4b6a62e1b84f2bb7c60857843f5eac761b4902
lelouch-404 <leodeiska@gmail.com>
2026-03-31 12:22:21 -0700

    reto numero 1

--- curso_python3.py
+++ curso_python3.py
@@ -6,48 +6,65 @@ ventana.geometry("300x200")
 
 
 
-label_nombre = tk.Label(ventana, text="Nombre:")
+label_nombre = tk.Label(ventana, text="Nombre:",font=("Arial", 14))
 label_nombre.grid(row=0, column=0)
 
 entrada_nombre = tk.Entry(ventana)
 entrada_nombre.grid(row=0,column=1)
 
-label_edad = tk.Label(ventana, text="Edad:")
+label_edad = tk.Label(ventana, text="Edad:",font=("Arial", 14))
 label_edad.grid(row=1,column=0)
 
 entrada_edad = tk.Entry(ventana)
 entrada_edad.grid(row=1,column=1)
 
-resultado = tk.Label(ventana, text="")
+resultado = tk.Label(ventana, text="",font=("Arial",14))
 resultado.grid(row=2, column=0, columnspan=2)
 
-def verificar():
-    nombre = entrada_nombre.get()
+def evaluar_edad(nombre,edad):
 
     if nombre.strip() == "":
-        resultado.config(text="Ingresa un nombre válido")
-        return
-
-
-    edad = entrada_edad.get()
-
+        return "nombre_error"
+    
     if edad.strip() == "":
-        resultado.config(text="ingrese un numero valido")
-        return
-
+        return "edad_vacia"
+    
     try:
         edad = int(edad)
-
-        if edad >= 18:
-            resultado.config(text=f"{nombre}, eres mayor de edad")
-        else:
-            resultado.config(text=f"{nombre}, eres menor de edad")
-
     except ValueError:
-        resultado.config(text="Edad inválida")
+        return "edad_invalida"
+    
+    if edad >= 18:
+        return "mayor"
+    else:
+        return "menor"
+    
+def verificar():
+    nombre = entrada_nombre.get()
+    edad = entrada_edad.get()
+
+    resultado_logica = evaluar_edad(nombre, edad)
+
+    if resultado_logica == "nombre_error":
+        resultado.config(text="Ingresa un nombre válido", fg="orange")
+
+    elif resultado_logica == "edad_vacia":
+        resultado.config(text="Ingresa una edad", fg="orange")
+
+    elif resultado_logica == "edad_invalida":
+        resultado.config(text="Ingresa un número válido", fg="orange")
+
+    elif resultado_logica == "mayor":
+        resultado.config(text=f"{nombre}, eres mayor de edad", fg="green")
+
+    elif resultado_logica == "menor":
+        resultado.config(text=f"{nombre}, eres menor de edad", fg="red")
 
 
-boton = tk.Button(ventana, text="Verificar", command=verificar)
+    
+
+
+boton = tk.Button(ventana, text="Verificar", command=verificar,width=25)
 boton.grid(row=3,column=0,columnspan=2,pady=10)
 
 ventana.mainloop()
